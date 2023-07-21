3d7b90a1809bd88796cbf9c3c6496c2df053982e
DevJake99 <jacobg4@uci.edu>
2023-07-20 22:57:43 -0700

    adding Logic to the functions

--- script.js
+++ script.js
@@ -9,18 +9,33 @@ var passwordText = document.querySelector("#password");
 
 function generatePassword(){
   
-  var specialCharacters = confirm("Does your Password Require Special Characters? Press OK for yes or Cancel for no");
+  var specialCharactersPrompt = confirm("Does your Password Require Special Characters? Press OK for yes or Cancel for no");
   var upperCase = confirm("Does Your Password Require Uppercase Letters? Choose OK for yes and Cancel for No");
   var lowerCase = confirm("Does Your Password Require Lowercase Letters? Choose OK for yes and Cancel for No")
   var password = "";
+  var tempPass ="";
   var pwLength = parseInt(prompt("Enter a Desired Password Length Between 8-128 Characters"));
   
-  if (pwLength >= 8 && pasworodLength <= 128){
-    for (i=0; i<pwLength; i++){
-      
+  if (pwLength >= 8 && pwLength <= 128){
+    tempPass += specialCharacters;
+    tempPass += upperCaseLetters;
+    tempPass += lowerCaseLetters;
+    tempPass += numbers;
+    console.log(password);
+    
+     for (i = 0; i < pwLength; i++){
+       var randomize = Math.floor(Math.random() * tempPass.legnth);
+       password+=tempPass[randomize];
+      }
+      return password;
+  } 
+
+  else {
+    pwLength();
   }
 
 }
+
 
 // Write password to the #password input
 function writePassword() {
