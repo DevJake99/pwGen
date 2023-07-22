1bdbcdbcb3b2b2dc0a7f688e04f102cb647c832e
DevJake99 <jacobg4@uci.edu>
2023-07-22 15:55:05 -0700

    ReadMe + Js file updates

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Password Generator Starter Code
+Here is the link to the Password Generator Web App : https://devjake99.github.io/pwGen/
+This web app generates a random password based on user inputted criteria such as length, uppercase,lowercase, etc
+
+## How to use
+To use this app, click on the Generate Password Button, then interact with the prompts to identify the criteria for your password
+After completing the prompts a randomly generated secure password will appear in the box! 
--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 // Assignment code here
+/*States the variabled that we will need to use for generation of 
+our password, it's like the "ingredients"*/
 const specialCharacters = ["!","@","#","$","%","^","&","*","(",")","~","`","?",".",",",">","<","'",'"',";",":","/","[","]","{","}","=","+","-","_",];
 const upperCaseLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 const lowerCaseLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
@@ -12,15 +14,26 @@ function generatePassword(){
   var specialCharactersPrompt = confirm("Does your Password Require Special Characters? Press OK for yes or Cancel for no");
   var upperCase = confirm("Does Your Password Require Uppercase Letters? Choose OK for yes and Cancel for No");
   var lowerCase = confirm("Does Your Password Require Lowercase Letters? Choose OK for yes and Cancel for No")
+  var nums = confirm("Does Your Password Require Numbers?");
   var password = "";
   var tempPass ="";
   var pwLength = parseInt(prompt("Enter a Desired Password Length Between 8-128 Characters"));
   
+  /* checks to see if desired length meets criteria of 8-128 characters*/
+  /* as well as if the criteria prompts are true*/
   if (pwLength >= 8 && pwLength <= 128){
-    tempPass += specialCharacters;
-    tempPass += upperCaseLetters;
-    tempPass += lowerCaseLetters;
-    tempPass += numbers;
+    if(specialCharactersPrompt){
+      tempPass += specialCharacters;
+    }
+    if (upperCase){
+      tempPass += upperCaseLetters;
+    }
+    if (lowerCase){
+      tempPass += lowerCaseLetters;
+    }
+    if(nums){
+      tempPass += numbers;
+    }
     console.log(password);
     
      for (i = 0; i < pwLength; i++){
@@ -31,17 +44,17 @@ function generatePassword(){
   } 
 
   else {
-    pwLength();
+    pwLength;
   }
 
 }
 
 
 // Write password to the #password input
-function writePassword() {
+function `writePassword`() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  passwordText.setAttribute("Placeholder", password) = password;
   passwordText.value = password;
 
 }
